afc1b85ff46d61c46c953b825a5edbda937b49ce
Mitchell Christ <mitchell@human.mareketing>
2024-07-31 14:34:42 -0700

    check feature

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,10 @@
 <h1>Notifications</h1>
+
+{#if 'Notifications' in window}
+	<p>Notifications are supported</p>
+{:else}
+	<p>Notifications are not supported in your browser.</p>
+{/if}
+
+<script lang="ts">
+</script>
