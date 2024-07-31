f14919588e399b55b5d626af23441dec2ce6fa57
Mitchell Christ <mitchell@human.mareketing>
2024-07-31 15:08:05 -0700

    test notification

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,21 @@
 <h1>Notifications</h1>
 
 {#if typeof window !== 'undefined' && 'Notification' in window}
-	<p>Notifications are supported</p>
+	{#await Notification.requestPermission()}
+		<p>Requesting permission to send notifications</p>
+	{:then permission}
+		{#if permission === 'granted'}
+			<button
+				on:click={() => {
+					new Notification('Hello world!')
+				}}
+			>
+				Notify
+			</button>
+		{:else}
+			<p>Notifications are disabled</p>
+		{/if}
+	{/await}
 {:else}
 	<p>Notifications are not supported in your browser.</p>
 {/if}
